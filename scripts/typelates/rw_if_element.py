b649fe2251183f9bfbd9d3543ee6b168fec35fd7
Rob McAuley <rmcauley@gmail.com>
2021-01-30 19:04:44 +0100

    Fix if elements

--- scripts/typelates/rw_if_element.py
+++ scripts/typelates/rw_if_element.py
@@ -5,29 +5,12 @@ class RainwaveIfElement(RainwaveElement):
     def get_if_clause(self) -> str:
         attrs = self.attrs
 
-        not_attr = next((attr for attr in attrs if attr[0] == "not"), None)
-        left = next((attr for attr in attrs if attr[0] == "left"), None)
-        op = next((attr for attr in attrs if attr[0] == "op"), None)
-        right = next((attr for attr in attrs if attr[0] == "right"), None)
+        condition = next((attr for attr in attrs if attr[0] == "condition"), None)
 
-        if not left:
-            raise Exception(f"if with no left")
+        if not condition:
+            raise Exception(f"if with no condition")
 
-        result = "("
-        if not_attr:
-            result += "!("
-        left_value = left[1]
-        result += f"{left_value}"
-        if op and right:
-            right_value = right[1]
-            result += f" {op} {right_value}"
-        elif (not op and right) or (op and not right):
-            raise Exception(f"invalid if")
-        result += ")"
-        if not_attr:
-            result += ")"
-
-        return result
+        return f"({condition[1]})"
 
     def handle_start(self) -> str:
         if_clause = self.get_if_clause()
